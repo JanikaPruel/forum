38221dff4516369e9641fd6bf35d565be9f4edee
Janika <prueljanika000@gmail.com>
2024-03-13 23:03:22 +0200

    assigned tasks

--- internal/app/app.go
+++ internal/app/app.go
@@ -11,4 +11,18 @@ func Run(cfg *config.Config) {
 
 	fmt.Printf("\n\n Server runnig on -> http://%s:%s\n\n Enter Ctrl + C for stop application", cfg.HTTPServer.Host, cfg.HTTPServer.Port)
 	// Perfect end our application
+
+	// prepare database for connection
+	// connection to database
+
+	// initrouter
+	// middleware
+	// controller
+	// http server
+
+	//start http server
+
+	// syscall signal
+	// close database connnection
+	// stop http server
 }
